5e707824b5038f7eb9ff1695b5229bc2d3a531ed
El Mahdi Amerian <mehdiamerian@gmail.com>
2024-09-19 08:49:56 +0300

    Add project files.

---
+++ FantasyRPG/GameWorld.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FantasyRPG
+{
+    public class GameWorld
+    {
+        private static GameWorld _instance = new GameWorld();
+        static GameWorld()
+        {
+
+        }
+
+        private GameWorld() 
+        {
+        
+        }
+
+        public static GameWorld Instance 
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+    }
+}
